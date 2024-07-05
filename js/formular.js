9228c883f516d1cc2d387cb1e28357cdb299bd87
razvanmihai <razvanmihai280@gmail.com>
2024-07-05 17:43:48 +0300

    rollback

--- js/formular.js
+++ js/formular.js
@@ -10,33 +10,6 @@ document.addEventListener('DOMContentLoaded', function() {
     var campMesaj = document.getElementById('campMesaj');
     var submitButton = document.getElementById('submit');
     var form = document.getElementById('contact-form');
-    var numarMeniuriStandard = document.querySelector("select[name='Numar Meniuri Standard']");
-    var numarMeniuriVegetarian = document.getElementById('numarMeniuriVegetarian');
-    var hiddenVegetarianMeniuri = document.getElementById('hiddenVegetarianMeniuri');
-    var numarCopiiSelect = document.querySelector("select[name='Numar Copii']");
-
-    function updateMeniuOptions() {
-        var numarPersoaneVal = parseInt(numarPersoane.value) || 0;
-        var numarCopiiVal = parseInt(numarCopiiSelect.value) || 0;
-        var totalMeniuri = numarPersoaneVal + numarCopiiVal;
-
-        var optionsHtml = '<option value="">-- Selectați o opțiune --</option>';
-        for (var i = 0; i <= totalMeniuri; i++) {
-            optionsHtml += `<option value="${i}">${i}</option>`;
-        }
-
-        numarMeniuriStandard.innerHTML = optionsHtml;
-        updateVegetarianMenu();
-    }
-
-    function updateVegetarianMenu() {
-        var totalMeniuri = (parseInt(numarPersoane.value) || 0) + (parseInt(numarCopiiSelect.value) || 0);
-        var standardMeniuri = parseInt(numarMeniuriStandard.value) || 0;
-        var vegetarianMeniuri = totalMeniuri - standardMeniuri;
-
-        hiddenVegetarianMeniuri.value = vegetarianMeniuri;
-        numarMeniuriVegetarian.innerHTML = `<option value="${vegetarianMeniuri}">${vegetarianMeniuri}</option>`;
-    }
 
     function validateForm() {
         var valid = true;
@@ -73,9 +46,15 @@ document.addEventListener('DOMContentLoaded', function() {
                 valid = false;
             }
 
-            if (campMeniu.style.display !== 'none' && numarMeniuriStandard.value === '') {
+            if (campMeniu.style.display !== 'none' && campMeniu.querySelector('select').value === '') {
                 valid = false;
             }
+
+            if (campVegan.style.display !== 'none' && campVegan.querySelector('select').value === '') {
+                valid = false;
+            }
+
+           
         }
 
         submitButton.style.display = valid ? 'block' : 'none';
@@ -87,22 +66,10 @@ document.addEventListener('DOMContentLoaded', function() {
         campInvitatPartener.style.display = 'none';
         if (numarPersoane.value === '1') {
             campInvitat.style.display = 'block';
-            campInvitatPartener.querySelector('input').value = ""; // Resetează câmpul partener
         } else if (numarPersoane.value === '2') {
             campInvitat.style.display = 'block';
             campInvitatPartener.style.display = 'block';
         }
-        updateMeniuOptions();
-        validateForm();
-    });
-
-    numarCopiiSelect.addEventListener('change', function() {
-        updateMeniuOptions();
-        validateForm();
-    });
-
-    numarMeniuriStandard.addEventListener('change', function() {
-        updateVegetarianMenu();
         validateForm();
     });
 
